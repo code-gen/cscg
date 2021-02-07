8115c2db80bde8d794e03347fffbcbb0429cee36
Alexandru Dinu <alex.dinu07@gmail.com>
2021-02-07 16:57:54 +0200

    Simplify glove emb loading.

--- loaders.py
+++ loaders.py
@@ -1,17 +1,12 @@
-import os
 import pickle
+from pathlib import Path
 
 
-def get_coefs(word, *arr):
-    return word, np.asarray(arr, dtype='float32')
+def load_pt_glove(emb_file: Path) -> dict:
+    def _get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
 
-
-def load_pt_glove(emb_file: str) -> dict:
-    if any([os.path.isfile(emb_file + ".pickle"), emb_file.endswith(".pickle")]):
-        emb_file = emb_file + ".pickle" if not emb_file.endswith(".pickle") else emb_file
-        emb_dict = pickle.load(open(emb_file, "rb"))
-    else:
-        emb_dict = dict(get_coefs(*o.split(" ")) for o in open(emb_file, encoding='latin'))
-        pickle.dump(emb_dict, open(emb_file + ".pickle", "wb"))
+    with open(emb_file, 'rb') as fp:
+        emb_dict = pickle.load(fp)
 
     return emb_dict
